8a366123c373385731411bdf2749db52d2f93f56
Victoria Palash <vika7kfc@gmail.com>
2022-06-23 22:49:45 +0300

    Added function getForecast

--- src/javascript.js
+++ src/javascript.js
@@ -52,7 +52,8 @@ function find(event) {
   axios.get(apiUrl).then(displayWeather);
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Wed", "Thu", "Fri", "Sat"];
@@ -74,6 +75,12 @@ function displayForecast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
+}
+
+function getForecast(coordinates) {
+  let apiKey = "c1f9fba0b38d3c38890fede647fd29cb";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayWeather(response) {
@@ -99,6 +106,7 @@ function displayWeather(response) {
     "src",
     `images/${response.data.weather[0].icon}.svg`
   );
+  getForecast(response.data.coord);
 }
 
 let searchForm = document.querySelector("#search-city");
@@ -155,5 +163,3 @@ function convertToCelsius(event) {
 }
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
-
-displayForecast();
