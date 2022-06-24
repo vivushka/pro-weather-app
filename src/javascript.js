91fcfdd36751e0fdc8f889c2e9e1fe2940b911b3
Victoria Palash <vika7kfc@gmail.com>
2022-06-24 20:35:21 +0300

    Added function formatDay

--- src/javascript.js
+++ src/javascript.js
@@ -52,25 +52,39 @@ function find(event) {
   axios.get(apiUrl).then(displayWeather);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Wed", "Thu", "Fri", "Sat"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 4) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="col-3">
-    <div class="weather-forecast-date">${day}</div>
-     <i class="fa-solid fa-sun"></i>
+    <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+     <img src="images/${forecastDay.weather[0].icon}.svg"
+          width="50"
+          />
      <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 20° </span>
-          <span class="weather-forecast-temperature-min"> 12° </span>
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
      </div> 
      </div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
